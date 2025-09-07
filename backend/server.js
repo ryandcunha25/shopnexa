fbcd3e1dd6366107ac8473dfe484d89f7473a826
ryandcunha25 <ryandcunha2812@gmail.com>
2025-09-08 00:06:36 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://shopnexa-rd.vercel.app", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/shopnexa_api/auth", authRoutes);
